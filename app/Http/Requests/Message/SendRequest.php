217d4c70c9a6422ceb28790f071c78f4b29f9b0e
a.salkov <a.salkov@dats.team>
2025-09-01 03:09:14 +0300

    Base logic

---
+++ app/Http/Requests/Message/SendRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Message;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class SendRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'receiver_id' => 'required|exists:users,id',
+            'content' => 'required|string',
+            'type' => 'sometimes|in:text,image,voice,video,file',
+        ];
+    }
+}
