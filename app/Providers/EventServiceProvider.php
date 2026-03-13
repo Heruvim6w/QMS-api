c9ffadbf3c95fc512a794576965f19d64b25263a
qualmii <salkov_andrei@mail.ru>
2026-03-13 05:21:01 +0300

    Remove event listener mappings from EventServiceProvider to adopt Laravel 12 configuration approach

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,32 +4,17 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Events\LoginConfirmed;
-use App\Events\LoginConfirmationRequested;
-use App\Events\UserRegistered;
-use App\Listeners\LogLoginConfirmed;
-use App\Listeners\SendLoginConfirmationEmail;
-use App\Listeners\SendRegistrationConfirmationEmail;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
     /**
      * The event listener mappings for the application.
+     * Listeners are registered via config/events.php (Laravel 12 approach).
      *
      * @var array<class-string, array<int, class-string>>
      */
-    protected $listen = [
-        UserRegistered::class => [
-            SendRegistrationConfirmationEmail::class,
-        ],
-        LoginConfirmationRequested::class => [
-            SendLoginConfirmationEmail::class,
-        ],
-        LoginConfirmed::class => [
-            LogLoginConfirmed::class,
-        ],
-    ];
+    protected $listen = [];
 
     /**
      * Register any events for your application.
